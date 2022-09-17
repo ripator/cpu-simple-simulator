e6a41015912207895d1d954a41010332db1b6e4e
ripator <ripatoroyan@gmail.com>
2022-09-17 20:37:00 +0400

    [FEATURE]: Add logic & arithmetic methods of CPU.

---
+++ instructions.js
@@ -0,0 +1,8 @@
+const instructions = `
+mov reg5, 10
+cmp 10, 10
+jle Test
+add reg5, 10
+Test:
+print reg5 
+`;
---
+++ registerNames.js
@@ -0,0 +1,14 @@
+const registerNames = {
+   reg0: 'reg0',
+   reg1: 'reg1',
+   reg2: 'reg2',
+   reg3: 'reg3',
+   reg4: 'reg4',
+   reg5: 'reg5',
+   reg6: 'reg6',
+   reg7: 'reg7',
+   reg8: 'reg8',
+   reg9: 'reg9',
+}
+
+module.exports = registerNames;
