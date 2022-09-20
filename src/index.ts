3e69daabac35233b8249d291680ba86a600b3b22
ripator <ripatoroyan@gmail.com>
2022-09-20 16:34:58 +0400

    [FIX]: Remove unnecessary comment.

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,4 @@ Test:
 print reg5
 `;
 
-mainCPU.parseTextToInstructions(instructions)
-
-// const express = require('express')
-// const app = express()
-// const port = 3001
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`);
-//     console.log(mainCPU.parseTextToInstructions(instructions));
-// })
+mainCPU.parseTextToInstructions(instructions);
